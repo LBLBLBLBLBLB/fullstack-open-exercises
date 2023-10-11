c4442fa8e8f22ae5fe6a400c492bf9f61e91cecf
Levani Birchadze <levanbirchadze@gmail.com>
2023-10-11 19:04:38 +0400

    the phonebook 2.6

---
+++ part2/the_phonebook/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addName = (event) => {
+    event.preventDefault();
+    const newPersonName = { name: newName };
+    setPersons([...persons, newPersonName]);
+    setNewName("");
+  };
+  const handleChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, i) => (
+        <p key={i}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
