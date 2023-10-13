62aab43759c0b1bb040ce3500f622bf6c6572fd7
Levani Birchadze <levanbirchadze@gmail.com>
2023-10-13 15:37:34 +0400

    phonebook saved into backend

--- part2/the_phonebook/src/App.jsx
+++ part2/the_phonebook/src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Person from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchedName, setSearchedName] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const addName = (event) => {
     event.preventDefault();
@@ -35,9 +37,13 @@ const App = () => {
       if (!newPersonEntry.name || !newPersonEntry.number) {
         alert("add all information");
       } else {
-        setPersons([...persons, newPersonEntry]);
-        setNewName("");
-        setNewNumber("");
+        axios
+          .post("http://localhost:3001/persons", newPersonEntry)
+          .then((response) => {
+            setPersons([...persons, response.data]);
+            setNewName("");
+            setNewNumber("");
+          });
       }
     }
   };
