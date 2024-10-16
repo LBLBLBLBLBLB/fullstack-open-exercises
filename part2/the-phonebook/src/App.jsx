a5f7587d37f8527a22ffdf56bc7f959cf69fbe27
levanib <levanbirchadze@gmail.com>
2024-10-16 17:01:05 +0400

    complete exercise 2.11 from part2 - Getting data from server

--- part2/the-phonebook/src/App.jsx
+++ part2/the-phonebook/src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 import Filter from "./components/Filter";
 import Form from "./components/Form";
 import Persons from "./components/Persons";
 
+import axios from "axios";
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchedName, setSearchedName] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => setPersons(response.data));
+  }, []);
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -75,7 +79,11 @@ const App = () => {
       />
 
       <h2>Numbers</h2>
-      <Persons filteredNames={filteredNames} />
+      {persons.length > 0 ? (
+        <Persons filteredNames={filteredNames} />
+      ) : (
+        <p>the phonebook is empty</p>
+      )}
     </>
   );
 };
