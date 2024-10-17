7a3e8d7724e1cdeef2816c864f755b53a49d4f43
levanib <levanbirchadze@gmail.com>
2024-10-18 01:37:16 +0400

    complete exercise 2.20 from part2 - Adding styles to React app

--- part2/countries/src/components/CountryDetailed.jsx
+++ part2/countries/src/components/CountryDetailed.jsx
@@ -1,14 +1,38 @@
+import { useState, useEffect } from "react";
+
+import countriesService from "../services/countries";
+
 const CountryDetailed = ({
   selectedCountry,
   setSelectedCountry,
   showBackButton = true,
 }) => {
+  const weatherImg = { backgroundColor: "lightgray" };
+
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      try {
+        const weatherData = await countriesService.getWeather(
+          selectedCountry.capital[0]
+        );
+        setWeather(weatherData);
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+      }
+    };
+
+    fetchWeather();
+  }, [selectedCountry]);
+
   const backToList = () => {
     setSelectedCountry(null);
   };
 
   return (
     <div>
+      {showBackButton && <button onClick={backToList}>Back</button>}
       <h2>{selectedCountry.name.common}</h2>
       <p>capital: {selectedCountry.capital}</p>
       <p>area: {selectedCountry.area}</p>
@@ -19,7 +43,23 @@ const CountryDetailed = ({
         ))}
       </ul>
       <img src={selectedCountry.flags.png} alt={selectedCountry.flags.alt} />
-      {showBackButton && <button onClick={backToList}>Back</button>}
+
+      {weather ? (
+        <div>
+          <p>
+            <strong>Weather in {selectedCountry.capital}</strong>
+          </p>
+          <p>Temperature: {weather.main.temp}°C</p>
+          <img
+            style={weatherImg}
+            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            alt={weather.weather[0].description}
+          />
+          <p>Wind: {weather.wind.speed} m/s</p>
+        </div>
+      ) : (
+        <p>Loading weather data...</p>
+      )}
     </div>
   );
 };
--- part2/countries/src/services/countries.js
+++ part2/countries/src/services/countries.js
@@ -2,10 +2,19 @@ import axios from "axios";
 
 const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api/all";
 
+const api_key = import.meta.env.VITE_SOME_KEY;
+
 const getAll = async () => {
-  const request = axios.get(baseUrl);
-  const response = (await request).data;
-  return response;
+  const response = await axios.get(baseUrl);
+
+  return response.data;
 };
 
-export default { getAll };
+const getWeather = async (capital) => {
+  const response = await axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}&units=metric`
+  );
+  return response.data;
+};
+
+export default { getAll, getWeather };
