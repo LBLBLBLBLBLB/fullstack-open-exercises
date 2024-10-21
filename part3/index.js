38393b215c474105d9db540a8f82d1c4f4d852d3
levanib <levanbirchadze@gmail.com>
2024-10-21 16:14:35 +0400

    complete exercise 3.7-3.8 from part3 - Node.js and Express

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,25 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
+
+app.use(morgan("tiny"));
+
+morgan.token("post-data", (request, response) => {
+  if (request.method === "POST") {
+    return JSON.stringify(request.body);
+  }
+  return "";
+});
+const postLog =
+  ":method :url :status :response-time ms - :res[content-length] :post-data";
+
+app.use(
+  morgan(postLog, {
+    skip: (request, response) => request.method !== "POST",
+  })
+);
 
 let persons = [
   {
@@ -55,7 +73,6 @@ app.get("/api/persons/:id", (request, response) => {
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
-  console.log(body);
 
   if (!body.name || !body.number) {
     return response.status(400).json({
